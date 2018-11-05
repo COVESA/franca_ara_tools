7f638a4fe250cd9c340e5d3dbad3daf488412552
Klaus Birken <klaus.birken@gmail.com>
2018-11-05 18:54:33 +0100

    Add initial packages and classes, remove errors (FARA23).

---
+++ plugins/org.franca.connectors.ara/src/org/franca/connectors/ara/ARAModelContainer.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2018 itemis AG (http://www.itemis.de).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.franca.connectors.ara;
+
+import org.franca.core.framework.IModelContainer;
+
+public class ARAModelContainer implements IModelContainer {
+//	private NodeType model = null;
+	
+	public ARAModelContainer(/*NodeType model*/) {
+//		this.model = model;
+	}
+	
+	public /*NodeType*/boolean model() {
+		return true;
+		//return model;
+	}
+	
+ }
