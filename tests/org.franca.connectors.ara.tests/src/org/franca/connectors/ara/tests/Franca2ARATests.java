cfdfe5ca75654b31a63c74bfc3f6eca4d9f7f788
Klaus Birken <klaus.birken@gmail.com>
2018-11-13 12:23:06 +0100

    FARA-29: Refactor test class.

--- tests/org.franca.connectors.ara.tests/src/org/franca/connectors/ara/tests/Franca2ARATests.java
+++ tests/org.franca.connectors.ara.tests/src/org/franca/connectors/ara/tests/Franca2ARATests.java
@@ -1,29 +1,20 @@
 package org.franca.connectors.ara.tests;
 
-import static org.junit.Assert.assertNotNull;
-
 import org.eclipse.xtext.testing.InjectWith;
-import org.franca.connectors.ara.ARAConnector;
-import org.franca.connectors.ara.ARAModelContainer;
+import org.franca.connectors.ara.tests.util.Franca2ARATestBase;
 import org.franca.core.dsl.FrancaIDLTestsInjectorProvider;
-import org.franca.core.dsl.FrancaPersistenceManager;
 import org.franca.core.dsl.tests.util.XtextRunner2_Franca;
-import org.franca.core.franca.FModel;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.google.inject.Inject;
-
 @RunWith(XtextRunner2_Franca.class)
 @InjectWith(FrancaIDLTestsInjectorProvider.class)
-public class Franca2ARATests {
+public class Franca2ARATests extends Franca2ARATestBase {
 
 	private static final String LOCAL_FRANCA_MODELS = "models/simple/";
 	private static final String REF_EXAMPLE_FRANCA_MODELS =
 			"../../examples/org.franca.examples.reference/models/org/reference/";
 
-	@Inject	FrancaPersistenceManager loader;
-	
 	@Test
 	public void test_20() {
 		doTransformTest(LOCAL_FRANCA_MODELS, "simple");
@@ -45,39 +36,5 @@ public class Franca2ARATests {
 //	}
 //
 
-	@SuppressWarnings("restriction")
-	private void doTransformTest (String path, String fileBasename) {
-		// load example Franca IDL interface
-		String inputfile = path + fileBasename + ".fidl";
-		System.out.println("Loading Franca file " + inputfile + " ...");
-		FModel fmodel = loader.loadModel(inputfile);
-		assertNotNull(fmodel);
-		
-		// transform to arxml
-		ARAConnector conn = new ARAConnector();
-		ARAModelContainer fromFranca = (ARAModelContainer)conn.fromFranca(fmodel);
-		conn.saveModel(fromFranca, "src-gen/testcases/" + fileBasename + ".arxml");
-		
-		// load reference arxml file
-		String referenceFile = "model/reference/" + fileBasename + ".arxml";
-		ARAModelContainer ref = (ARAModelContainer)conn.loadModel(referenceFile);
-		
-		// compare with reference file
-//		ResourceSet rset1 = fromFranca.model().eResource().getResourceSet();
-//		ResourceSet rset2 = ref.model().eResource().getResourceSet();
-//
-//		IComparisonScope scope = EMFCompare.createDefaultScope(rset1, rset2);
-//		Comparison comparison = EMFCompare.builder().build().compare(scope);
-//		 
-//		List<Diff> differences = comparison.getDifferences();
-//		int nDiffs = 0;
-//		for (Diff diff : differences) {
-//			if (! (diff instanceof ResourceAttachmentChangeSpec)) {
-//				System.out.println(diff.toString());
-//				nDiffs++;
-//			}
-//		}
-//		assertEquals(0, nDiffs);
-	}
 }
 
---
+++ tests/org.franca.connectors.ara.tests/src/org/franca/connectors/ara/tests/util/Franca2ARATestBase.xtend
@@ -0,0 +1,48 @@
+package org.franca.connectors.ara.tests.util
+
+import org.franca.connectors.ara.ARAModelContainer
+import org.franca.connectors.ara.ARAConnector
+import org.franca.core.franca.FModel
+import static org.junit.Assert.assertNotNull
+import org.franca.core.dsl.FrancaPersistenceManager
+import com.google.inject.Inject
+
+class Franca2ARATestBase {
+
+	@Inject	FrancaPersistenceManager loader
+
+	@SuppressWarnings("restriction")
+	def void doTransformTest (String path, String fileBasename) {
+		// load example Franca IDL interface
+		val inputfile = path + fileBasename + ".fidl"
+		println("Loading Franca file " + inputfile + " ...")
+		val fmodel = loader.loadModel(inputfile)
+		assertNotNull(fmodel)
+		
+		// transform to arxml
+		val conn = new ARAConnector
+		val fromFranca = conn.fromFranca(fmodel) as ARAModelContainer
+		conn.saveModel(fromFranca, "src-gen/testcases/" + fileBasename + ".arxml")
+		
+		// load reference arxml file
+		val referenceFile = "model/reference/" + fileBasename + ".arxml"
+		val ref = conn.loadModel(referenceFile) as ARAModelContainer
+		
+		// compare with reference file
+//		val rset1 = fromFranca.model.eResource.resourceSet
+//		val rset2 = ref.model.eResource.resourceSet
+//
+//		val IComparisonScope scope = EMFCompare.createDefaultScope(rset1, rset2)
+//		val Comparison comparison = EMFCompare.builder.build.compare(scope)
+//		 
+//		val List<Diff> differences = comparison.getDifferences
+//		val nDiffs = 0
+//		for(Diff diff : differences) {
+//			if (! (diff instanceof ResourceAttachmentChangeSpec)) {
+//				println(diff.toString);
+//				nDiffs++
+//			}
+//		}
+//		assertEquals(0, nDiffs)
+	}
+}
