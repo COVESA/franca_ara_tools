fe1cd3c0d250b4366bc4c245a40b58bd84dfa8f8
Torsten Görg <tgoerg@itemis.de>
2019-06-18 11:44:25 +0200

    Issue #21: Implemented initial structuring of test cases.

---
+++ tests/org.franca.connectors.ara.tests/src/org/franca/connectors/ara/tests/aspects_on_file_level/a2f/IDL1140_Tests.java
@@ -0,0 +1,13 @@
+package org.franca.connectors.ara.tests.aspects_on_file_level.a2f;
+
+import org.eclipse.xtext.testing.InjectWith;
+import org.franca.connectors.ara.tests.util.ARA2FrancaTestBase;
+import org.franca.core.dsl.FrancaIDLTestsInjectorProvider;
+import org.franca.core.dsl.tests.util.XtextRunner2_Franca;
+import org.junit.runner.RunWith;
+
+@RunWith(XtextRunner2_Franca.class)
+@InjectWith(FrancaIDLTestsInjectorProvider.class)
+public class IDL1140_Tests extends ARA2FrancaTestBase {
+	
+}
