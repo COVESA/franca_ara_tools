e2528bf0358311be731e8a88aca5312f1cb4ed72
Torsten Görg <tgoerg@itemis.de>
2019-07-01 23:14:03 +0200

    Issue #10: Adapted the command line options to the specification of "faracon".

--- plugins/org.genivi.faracon.cli/src/org/genivi/faracon/cli/CommandLineHandler.java
+++ plugins/org.genivi.faracon.cli/src/org/genivi/faracon/cli/CommandLineHandler.java
@@ -13,8 +13,9 @@ import org.genivi.faracon.console.ICommandLineHandler;
 public class CommandLineHandler extends AbstractCommandLineHandler implements ICommandLineHandler
 {
 
-    public static final String      FILE_EXTENSION_FDEPL = "fdepl";
-    public static final String      FILE_EXTENSION_FIDL  = "fidl";
+//    public static final String FILE_EXTENSION_FDEPL = "fdepl";
+//    public static final String FILE_EXTENSION_FIDL  = "fidl";
+
     private CommandlineToolMain cliTool;
 
     public CommandLineHandler()
@@ -24,103 +25,61 @@ public class CommandLineHandler extends AbstractCommandLineHandler implements IC
 
     @Override
 	public int excute(CommandLine parsedArguments) {
-		@SuppressWarnings("unchecked")
-		List<String> files = parsedArguments.getArgList();
-		// a search path may be specified, collect all fidl/fdepl files
-		if (parsedArguments.hasOption("sp")) {
-			files.addAll(cliTool.searchFidlandFdeplFiles(parsedArguments
-					.getOptionValue("sp")));
+    	println("Command: Franca ARA Converter");
+    	
+		// Handle command line options.
+
+//		@SuppressWarnings("unchecked")
+
+		List<String> commandLineArguments = parsedArguments.getArgList();
+		if (!commandLineArguments.isEmpty()) {
+			//TODO: error handling
 		}
-		// We expect at least one fidl/fdepl file as command line argument
-		if (files.size() > 0 && files.get(0) != null) {
-			String file = files.get(0);
-			// handle command line options
 
-			// -ll --loglevel quiet or verbose
-			if (parsedArguments.hasOption("ll")) {
-				cliTool.setLogLevel(parsedArguments.getOptionValue("ll"));
-			}
-			ConsoleLogger
-					.printLog("Executing Franca ARA Conversion...\n");
+		String[] francaFilePaths = parsedArguments.getOptionValues('f');
+		String[] araFilePaths = parsedArguments.getOptionValues('a');
+//		// We expect at least one fidl/fdepl file as command line argument
+//		if (files.size() > 0 && files.get(0) != null) {
+		if ((francaFilePaths == null || francaFilePaths.length == 0) && (araFilePaths == null || araFilePaths.length == 0)) {
+			//TODO: error handling
+//			System.out.println("A *.fidl or *.fdepl file was not specified !");
+		}		
+//		// a search path may be specified, collect all fidl/fdepl files
+//		if (parsedArguments.hasOption("sp")) {
+//			files.addAll(cliTool.searchFidlandFdeplFiles(parsedArguments
+//					.getOptionValue("sp")));
+//		}
 
-			// Switch off generation of common code
-			// -nc --no-common do not generate proxy code
-			if (parsedArguments.hasOption("nc")) {
-				cliTool.setNoCommonCode();
-			}
+		// destination: -d --dest overwrite default output directory path
+		if (parsedArguments.hasOption("d")) {
+			cliTool.setOutputDirectoryPath(parsedArguments.getOptionValue("d"));
+		}
 
-			// Switch off generation of proxy code
-			// -np --no-proxy do not generate proxy code
-			if (parsedArguments.hasOption("np")) {
-				cliTool.setNoProxyCode();
-			}
+		// A file path, that points to a file, that contains the license text.
+		// -L --license license text in generated files
+		if (parsedArguments.hasOption("L")) {
+			cliTool.setLicenseText(parsedArguments.getOptionValue("L"));
+		}
 
-			// Switch off generation of stub code
-			// -ns --no-stub do not generate stub code
-			if (parsedArguments.hasOption("ns")) {
-				cliTool.setNoStubCode();
-			}
+		// -l --log-level quiet or verbose
+		if (parsedArguments.hasOption("l")) {
+			cliTool.setLogLevel(parsedArguments.getOptionValue("l"));
+		}
 
-			// destination: -d --dest overwrite default directory
-			if (parsedArguments.hasOption("d")) {
-				cliTool.setDefaultDirectory(parsedArguments.getOptionValue("d"));
-			}
+//			// Switch off validation
+//			if (parsedArguments.hasOption("nv")) {
+//				cliTool.disableValidation();
+//			}
 
-			// destination: -dsub --dest-subdirs use destination/<filename>/ subdirs
-			// as dest directory
-			if(parsedArguments.hasOption("dsub")) {
-				cliTool.setDestinationSubdirs();
-			}
-
-			// destination: -dc --dest-common overwrite target directory for
-			// common part
-			if (parsedArguments.hasOption("dc")) {
-				cliTool.setCommonDirectory(parsedArguments.getOptionValue("dc"));
-			}
-
-			// destination: -dp --dest-proxy overwrite target directory for
-			// proxy code
-			if (parsedArguments.hasOption("dp")) {
-				cliTool.setProxyDirectory(parsedArguments.getOptionValue("dp"));
-			}
-
-			// destination: -ds --dest-stub overwrite target directory for stub
-			// code
-			if (parsedArguments.hasOption("ds")) {
-				cliTool.setStubDirectory(parsedArguments.getOptionValue("ds"));
-			}
-
-			// A file path, that points to a file, that contains the license
-			// text.
-			// -l --license license text in generated files
-			if (parsedArguments.hasOption("l")) {
-				cliTool.setLicenseText(parsedArguments.getOptionValue("l"));
-			}
-
-			// Switch off validation
-			if (parsedArguments.hasOption("nv")) {
-				cliTool.disableValidation();
-			}
-			// Switch off code generation at all
-			if (parsedArguments.hasOption("ng")) {
-				cliTool.disableCodeGeneration();
-			}
-			// Don't generate code for included types and interfaces
-			if (parsedArguments.hasOption("wod")) {
-				cliTool.noCodeforDependencies();
-			}
-			// Don't generate synchronous calls
-			if (parsedArguments.hasOption("nsc")) {
-				cliTool.disableSyncCalls();
-			}
-			// print out generated files
-			if (parsedArguments.hasOption("pf")) {
-				cliTool.listGeneratedFiles();
-			}
-			// finally invoke the generator.
-			return cliTool.generateDBus(files);
-		} else {
-			System.out.println("A *.fidl or *.fdepl file was not specified !");
+		// Invoke the converters.
+		ConsoleLogger.printLog("Franca ARA Converter");
+		int result = cliTool.convertFrancaFiles(francaFilePaths);
+		if (result == 0) {
+			return result;
+		}
+		result = cliTool.convertARAFiles(araFilePaths);
+		if (result == 0) {
+			return result;
 		}
 		return 0;
 	}
