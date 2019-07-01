51e0c2b7924201e7a3625867054d90531320d796
Torsten Görg <tgoerg@itemis.de>
2019-07-01 23:15:27 +0200

    Issue #10: Adapted the preferences parameters to the modified command line options.

--- plugins/org.genivi.faracon/src/org/genivi/faracon/preferences/Preferences.java
+++ plugins/org.genivi.faracon/src/org/genivi/faracon/preferences/Preferences.java
@@ -36,23 +36,11 @@ public class Preferences {
 
 	    private void clidefPreferences(){
 
-	        if (!preferences.containsKey(PreferencesConstants.P_OUTPUT_COMMON_DBUS)) {
-	            preferences.put(PreferencesConstants.P_OUTPUT_COMMON_DBUS, PreferencesConstants.DEFAULT_OUTPUT);
+	        if (!preferences.containsKey(PreferencesConstants.P_OUTPUT_DIRECTORY_PATH)) {
+	            preferences.put(PreferencesConstants.P_OUTPUT_DIRECTORY_PATH, PreferencesConstants.DEFAULT_OUTPUT);
 	        }    	
-	        if (!preferences.containsKey(PreferencesConstants.P_OUTPUT_PROXIES_DBUS)) {
-	            preferences.put(PreferencesConstants.P_OUTPUT_PROXIES_DBUS, PreferencesConstants.DEFAULT_OUTPUT);
-	        }
-	        if (!preferences.containsKey(PreferencesConstants.P_OUTPUT_STUBS_DBUS)) {
-	            preferences.put(PreferencesConstants.P_OUTPUT_STUBS_DBUS, PreferencesConstants.DEFAULT_OUTPUT);
-	        }
-	        if (!preferences.containsKey(PreferencesConstants.P_OUTPUT_DEFAULT_DBUS)) {
-	            preferences.put(PreferencesConstants.P_OUTPUT_DEFAULT_DBUS, PreferencesConstants.DEFAULT_OUTPUT);
-	        }    
-	        if (!preferences.containsKey(PreferencesConstants.P_OUTPUT_SUBDIRS_DBUS)) {
-	            preferences.put(PreferencesConstants.P_OUTPUT_SUBDIRS_DBUS, "false");
-	        }
-	        if (!preferences.containsKey(PreferencesConstants.P_LICENSE_DBUS)) {
-	            preferences.put(PreferencesConstants.P_LICENSE_DBUS, PreferencesConstants.DEFAULT_LICENSE);
+	        if (!preferences.containsKey(PreferencesConstants.P_LICENSE)) {
+	            preferences.put(PreferencesConstants.P_LICENSE, PreferencesConstants.DEFAULT_LICENSE);
 	        }
 	        if (!preferences.containsKey(PreferencesConstants.P_GENERATE_COMMON_DBUS)) {
 	            preferences.put(PreferencesConstants.P_GENERATE_COMMON_DBUS, "true");
@@ -63,18 +51,9 @@ public class Preferences {
 	        if (!preferences.containsKey(PreferencesConstants.P_GENERATE_PROXY_DBUS)) {
 	            preferences.put(PreferencesConstants.P_GENERATE_PROXY_DBUS, "true");
 	        }
-	        if (!preferences.containsKey(PreferencesConstants.P_LOGOUTPUT_DBUS)) {
-	            preferences.put(PreferencesConstants.P_LOGOUTPUT_DBUS, "true");
+	        if (!preferences.containsKey(PreferencesConstants.P_LOGOUTPUT)) {
+	            preferences.put(PreferencesConstants.P_LOGOUTPUT, "true");
 	        }	        
-	        if (!preferences.containsKey(PreferencesConstants.P_GENERATE_CODE_DBUS)) {
-	            preferences.put(PreferencesConstants.P_GENERATE_CODE_DBUS, "true");    
-	        }
-	        if (!preferences.containsKey(PreferencesConstants.P_GENERATE_DEPENDENCIES_DBUS)) {
-	            preferences.put(PreferencesConstants.P_GENERATE_DEPENDENCIES_DBUS, "true");    
-	        }
-	        if (!preferences.containsKey(PreferencesConstants.P_GENERATE_SYNC_CALLS_DBUS)) {
-	            preferences.put(PreferencesConstants.P_GENERATE_SYNC_CALLS_DBUS, "true");    
-	        }
 	    }
 
 	    public String getPreference(String preferencename, String defaultValue) {
@@ -97,56 +76,4 @@ public class Preferences {
 	        return ret;
 	    }
 	    
-	    /**
-	     * Set the output path configurations (based on stored the preference values) for file system access types 
-	     * (instance of AbstractFileSystemAccess)
-	     * @return
-	     */
-	    public HashMap<String, OutputConfiguration> getOutputpathConfiguration() {
-	        return getOutputpathConfiguration(null);
-	    }
-
-	    /**
-	     * Set the output path configurations (based on stored the preference values) for file system access types
-	     * (instance of AbstractFileSystemAccess)
-			 * @subdir the subdir to use, can be null
-	     * @return
-	     */
-	    public HashMap<String, OutputConfiguration> getOutputpathConfiguration(String subdir) {
-
-	        String defaultDir = getPreference(PreferencesConstants.P_OUTPUT_DEFAULT_DBUS, PreferencesConstants.DEFAULT_OUTPUT);
-	        String commonDir = getPreference(PreferencesConstants.P_OUTPUT_COMMON_DBUS, defaultDir);
-	        String outputProxyDir = getPreference(PreferencesConstants.P_OUTPUT_PROXIES_DBUS, defaultDir);
-	        String outputStubDir = getPreference(PreferencesConstants.P_OUTPUT_STUBS_DBUS, defaultDir);
-
-	        if (null != subdir && getPreference(PreferencesConstants.P_OUTPUT_SUBDIRS, "false").equals("true")) {
-	            defaultDir = new File(defaultDir, subdir).getPath();
-	            commonDir = new File(commonDir, subdir).getPath();
-	            outputProxyDir = new File(outputProxyDir, subdir).getPath();
-	            outputStubDir = new File(outputStubDir, subdir).getPath();
-	        }
-
-	        HashMap<String, OutputConfiguration>  outputs = new HashMap<String, OutputConfiguration> ();
-	        
-	        OutputConfiguration commonOutput = new OutputConfiguration(PreferencesConstants.P_OUTPUT_COMMON_DBUS);
-	        commonOutput.setDescription("Common Output Folder");
-	        commonOutput.setOutputDirectory(commonDir);
-	        commonOutput.setCreateOutputDirectory(true);
-	        outputs.put(IFileSystemAccess.DEFAULT_OUTPUT, commonOutput);
-	        
-	        OutputConfiguration proxyOutput = new OutputConfiguration(PreferencesConstants.P_OUTPUT_PROXIES_DBUS);
-	        proxyOutput.setDescription("Proxy Output Folder");
-	        proxyOutput.setOutputDirectory(outputProxyDir);
-	        proxyOutput.setCreateOutputDirectory(true);
-	        outputs.put(PreferencesConstants.P_OUTPUT_PROXIES_DBUS, proxyOutput);
-	        
-	        OutputConfiguration stubOutput = new OutputConfiguration(PreferencesConstants.P_OUTPUT_STUBS_DBUS);
-	        stubOutput.setDescription("Stub Output Folder");
-	        stubOutput.setOutputDirectory(outputStubDir);
-	        stubOutput.setCreateOutputDirectory(true);
-	        outputs.put(PreferencesConstants.P_OUTPUT_STUBS_DBUS, stubOutput);
-	        
-	        return outputs;
-	    }
-
 }
--- plugins/org.genivi.faracon/src/org/genivi/faracon/preferences/PreferencesConstants.java
+++ plugins/org.genivi.faracon/src/org/genivi/faracon/preferences/PreferencesConstants.java
@@ -8,17 +8,12 @@ public interface PreferencesConstants
 
     // preference keys
     public static final String P_LICENSE          		= "licenseHeader";
-    public static final String P_OUTPUT_PROXIES   		= "outputDirProxies";
-    public static final String P_OUTPUT_STUBS     		= "outputDirStubs";
-	public static final String P_OUTPUT_COMMON    		= "outputDirCommon";
-	public static final String P_OUTPUT_SKELETON  		= "outputDirSkeleton";
-	public static final String P_OUTPUT_DEFAULT   		= "outputDirDefault";
-	public static final String P_OUTPUT_SUBDIRS   		= "outputSubdirs";
+    public static final String P_OUTPUT_DIRECTORY_PATH	= "outputDirectoryPath";
 	public static final String P_GENERATE_COMMON  		= "generatecommon";
     public static final String P_GENERATE_PROXY    		= "generateproxy";
     public static final String P_GENERATE_STUB     		= "generatestub";
     public static final String P_GENERATE_SKELETON 		= "generateskeleton";
-	public static final String P_LOGOUTPUT        		= "logoutput";
+	public static final String P_LOGOUTPUT        		= "logOutput";
 	public static final String P_ENUMPREFIX       		= "enumprefix";
 	public static final String P_SKELETONPOSTFIX  		= "skeletonpostfix";
 	public static final String P_USEPROJECTSETTINGS 	= "useProjectSettings";
@@ -28,7 +23,7 @@ public interface PreferencesConstants
     public static final String P_ENABLE_CORE_VALIDATOR 	= "enableCoreValidator";
 
 	// preference values
-    public static final String DEFAULT_OUTPUT     = "./src-gen/";
+    public static final String DEFAULT_OUTPUT     = ".";
 	public static final String LOGLEVEL_QUIET     = "quiet";
 	public static final String LOGLEVEL_VERBOSE   = "verbose";
     public static final String DEFAULT_LICENSE    = "This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.\n"
@@ -39,16 +34,9 @@ public interface PreferencesConstants
 
 
 	// preference keys
-    public static final String P_LICENSE_DBUS        	= P_LICENSE;
-    public static final String P_OUTPUT_PROXIES_DBUS 	= P_OUTPUT_PROXIES;
-    public static final String P_OUTPUT_STUBS_DBUS   	= P_OUTPUT_STUBS;
-	public static final String P_OUTPUT_COMMON_DBUS     = P_OUTPUT_COMMON;
-	public static final String P_OUTPUT_DEFAULT_DBUS	= P_OUTPUT_DEFAULT;
-	public static final String P_OUTPUT_SUBDIRS_DBUS	= P_OUTPUT_SUBDIRS;
     public static final String P_GENERATE_COMMON_DBUS	= P_GENERATE_COMMON;
     public static final String P_GENERATE_PROXY_DBUS	= P_GENERATE_PROXY;
     public static final String P_GENERATE_STUB_DBUS     = P_GENERATE_STUB;
-	public static final String P_LOGOUTPUT_DBUS        	= P_LOGOUTPUT;
 	public static final String P_USEPROJECTSETTINGS_DBUS= P_USEPROJECTSETTINGS;
 	public static final String P_GENERATE_CODE_DBUS     = P_GENERATE_CODE;
 	public static final String P_GENERATE_DEPENDENCIES_DBUS = P_GENERATE_DEPENDENCIES;
