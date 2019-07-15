fd033dbcea18c56c65ddaccc47e3ab18083e8144
Klaus Birken <klaus.birken@gmail.com>
2019-07-15 20:55:59 +0200

    Add dummy test to each test class; maven build fails on empty test classes.

--- tests/org.genivi.faracon.tests/src/org/genivi/faracon/tests/aspects_on_file_level/f2a/IDL1140_Tests.java
+++ tests/org.genivi.faracon.tests/src/org/genivi/faracon/tests/aspects_on_file_level/f2a/IDL1140_Tests.java
@@ -4,10 +4,14 @@ import org.eclipse.xtext.testing.InjectWith;
 import org.genivi.faracon.tests.util.Franca2ARATestBase;
 import org.franca.core.dsl.FrancaIDLTestsInjectorProvider;
 import org.franca.core.dsl.tests.util.XtextRunner2_Franca;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(XtextRunner2_Franca.class)
 @InjectWith(FrancaIDLTestsInjectorProvider.class)
 public class IDL1140_Tests extends Franca2ARATestBase {
-	
+	@Test
+	public void test1() {
+		
+	}
 }
