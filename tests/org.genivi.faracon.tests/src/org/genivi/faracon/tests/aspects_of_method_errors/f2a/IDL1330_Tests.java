eca7defbd4daa0bf9d5ed45935868f4fb6cc94e6
Torsten Görg <tgoerg@itemis.de>
2019-08-29 17:19:52 +0200

    Issue #44: Created test cases for the handling of Franca method error enumerations.

---
+++ tests/org.genivi.faracon.tests/src/org/genivi/faracon/tests/aspects_of_method_errors/f2a/IDL1330_Tests.java
@@ -0,0 +1,25 @@
+package org.genivi.faracon.tests.aspects_of_method_errors.f2a;
+
+import org.eclipse.xtext.testing.InjectWith;
+import org.franca.core.dsl.tests.util.XtextRunner2_Franca;
+import org.genivi.faracon.logging.AbstractLogger;
+import org.genivi.faracon.tests.util.FaraconTestsInjectorProvider;
+import org.genivi.faracon.tests.util.Franca2ARATestBase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Test cases for testing the conversion of The Franca "manages" relation between inheritance definitions.
+ */
+@RunWith(XtextRunner2_Franca.class)
+@InjectWith(FaraconTestsInjectorProvider.class)
+public class IDL1330_Tests extends Franca2ARATestBase {
+
+	private static final String LOCAL_FRANCA_MODELS = "src/org/genivi/faracon/tests/aspects_of_method_errors/f2a/";
+
+	@Test(expected = AbstractLogger.StopOnErrorException.class)
+	public void separateMethodErrorsEnumeration() {
+		transformAndCheck(LOCAL_FRANCA_MODELS, "separateMethodErrorsEnumeration");
+	}
+
+}
